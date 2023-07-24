b2f0d28b1579f0bf524f13ae8d01539639e4e1d0
mojomomo <2428015329@qq.com>
2023-07-24 08:57:32 +0800

    removeRestful

--- src/main/java/com/example/unityauth/config/SecurityConfig.java
+++ src/main/java/com/example/unityauth/config/SecurityConfig.java
@@ -1,66 +1,176 @@
 package com.example.unityauth.config;
-
-import com.example.unityauth.Fliter.CusUsernamePasswordAuthentication;
-import com.example.unityauth.Service.UserDetailsServiceImpl;
-import com.example.unityauth.hander.CusAuthenticationFailureHandler;
-import com.example.unityauth.hander.CusAuthenticationSuccessHandler;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.ProviderManager;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.core.oidc.OidcScopes;
+import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
+import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.stereotype.Component;
+import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
 
-import java.util.Collections;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+import java.util.UUID;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-    @Autowired
-    UserDetailsServiceImpl usersDetailsServices;
-    @Autowired
-    CusAuthenticationSuccessHandler cusAuthenticationSuccessHandler;
-    @Autowired
-    CusAuthenticationFailureHandler cusAuthenticationFailureHandler;
 
+    /**
+     * 这是个Spring security 的过滤器链，默认会配置
+     * <p>
+     * OAuth2 Authorization endpoint
+     * <p>
+     * OAuth2 Token endpoint
+     * <p>
+     * OAuth2 Token Introspection endpoint
+     * <p>
+     * OAuth2 Token Revocation endpoint
+     * <p>
+     * OAuth2 Authorization Server Metadata endpoint
+     * <p>
+     * JWK Set endpoint
+     * <p>
+     * OpenID Connect 1.0 Provider Configuration endpoint
+     * <p>
+     * OpenID Connect 1.0 UserInfo endpoint
+     * 这些协议端点，只有配置了他才能够访问的到接口地址（类似mvc的controller）。
+     *
+     * @param http
+     * @return
+     * @throws Exception
+     */
     @Bean
-    public Object passwordEncoder(){
-        return  PasswordEncoderFactories.createDelegatingPasswordEncoder();
-    }
-    @Bean
-    public CusUsernamePasswordAuthentication cusUsernamePasswordAuthentication() throws Exception {
-        CusUsernamePasswordAuthentication filter = new CusUsernamePasswordAuthentication();
-        // 创建 DaoAuthenticationProvider 作为认证提供者
-        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
-        authenticationProvider.setUserDetailsService(usersDetailsServices); // 设置自定义的 UserDetailsService
-        ProviderManager providerManager = new ProviderManager(Collections.singletonList(authenticationProvider));
-        filter.setAuthenticationSuccessHandler(cusAuthenticationSuccessHandler);
-        filter.setAuthenticationFailureHandler(cusAuthenticationFailureHandler);
-        filter.setAuthenticationManager(providerManager);
-        filter.setFilterProcessesUrl("/login"); // 设置登录接口 URL
-        return  filter;
-    }
-    @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http .authorizeHttpRequests((authz) -> authz.requestMatchers("test").permitAll()
-                        .anyRequest().authenticated()).csrf(csrf->csrf.disable())
-//                .formLogin(Customizer.withDefaults())
-                .addFilterAt(cusUsernamePasswordAuthentication(), UsernamePasswordAuthenticationFilter.class);
-        ;
+    @Order(1)
+    public SecurityFilterChain authorizationServerSecurityFilterChain(HttpSecurity http)
+            throws Exception {
+        OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(http);
+        http
+                // Redirect to the login page when not authenticated from the
+                // authorization endpoint
+                .exceptionHandling((exceptions) -> exceptions
+                        .authenticationEntryPoint(
+                                new LoginUrlAuthenticationEntryPoint("/login"))
+                );
 
-        // 允许登录接口通过认证
-
-        // 其他接口需要身份验证
-
-//         关闭CSRF保护,基于token
         return http.build();
-
-    }
     }
 
+    /**
+     * 这个也是个Spring Security的过滤器链，用于Spring Security的身份认证。
+     * @param http
+     * @return
+     * @throws Exception
+     */
+    @Bean
+    @Order(2)
+    public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http)
+            throws Exception {
+        http
+                .authorizeHttpRequests((authorize) -> authorize
+                        .anyRequest().authenticated()
+                )
+                // Form login handles the redirect to the login page from the
+                // authorization server filter chain
+                .formLogin(Customizer.withDefaults());
 
+        return http.build();
+    }
 
+    /**
+     * 配置用户信息，或者配置用户数据来源，主要用于用户的检索。
+     * @return
+     */
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails userDetails = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("passwords")
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(userDetails);
+    }
+
+    /**
+     * oauth2 用于第三方认证，RegisteredClientRepository 主要用于管理第三方（每个第三方就是一个客户端）
+     * @return
+     */
+    @Bean
+    public RegisteredClientRepository registeredClientRepository() {
+        RegisteredClient registeredClient = RegisteredClient.withId(UUID.randomUUID().toString())
+                .clientId("messaging-client")
+                .clientSecret("{noop}secret")
+                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
+                .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
+                .redirectUri("http://127.0.0.1:8080/login/oauth2/code/messaging-client-oidc")
+                .redirectUri("http://127.0.0.1:5500/authorized.html")
+                .scope(OidcScopes.OPENID)
+                .scope("message.read")
+                .scope("message.write")
+                .clientSettings(ClientSettings.builder().requireAuthorizationConsent(true).build())
+                .build();
+
+        return new InMemoryRegisteredClientRepository(registeredClient);
+    }
+
+    /**
+     * 用于给access_token签名使用。
+     * @return
+     */
+    @Bean
+    public JWKSource<SecurityContext> jwkSource() {
+        KeyPair keyPair = generateRsaKey();
+        RSAPublicKey publicKey = (RSAPublicKey) keyPair.getPublic();
+        RSAPrivateKey privateKey = (RSAPrivateKey) keyPair.getPrivate();
+        RSAKey rsaKey = new RSAKey.Builder(publicKey)
+                .privateKey(privateKey)
+                .keyID(UUID.randomUUID().toString())
+                .build();
+        JWKSet jwkSet = new JWKSet(rsaKey);
+        return new ImmutableJWKSet<>(jwkSet);
+    }
+
+    /**
+     * 生成秘钥对，为jwkSource提供服务。
+     * @return
+     */
+    private static KeyPair generateRsaKey() {
+        KeyPair keyPair;
+        try {
+            KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+            keyPairGenerator.initialize(2048);
+            keyPair = keyPairGenerator.generateKeyPair();
+        } catch (Exception ex) {
+            throw new IllegalStateException(ex);
+        }
+        return keyPair;
+    }
+
+    /**
+     * 配置Authorization Server实例
+     * @return
+     */
+
+}
